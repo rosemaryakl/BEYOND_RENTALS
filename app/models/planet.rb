1566398c9c80fb6e358b2e661e2172aff8844650
Michael Knott <knottweb@gmail.com>
2020-07-30 21:58:59 +0100

    Fixed some validations

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -3,5 +3,5 @@ class Planet < ApplicationRecord
   has_many :bookings, dependent: :destroy
   validates :name, :size, :distance_from_earth, :price_per_day, presence: true
   validates_uniqueness_of :name, on: :create, message: "must be unique"
-  validates :size, :distance_from_earth, :price_per_day, on: :create, message: "is not a number", numericality: { only_integer: true }
+  validates :size, :distance_from_earth, :price_per_day, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_30_202904) do
+ActiveRecord::Schema.define(version: 2020_07_30_203205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
