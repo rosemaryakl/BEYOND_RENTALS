e9bd4c6fdd1e74329d133efc34e4095410b09b04
Michael Knott <knottweb@gmail.com>
2020-07-30 21:43:19 +0100

    Changed spelling mistake

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,7 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  valdidates :name, :size, :distance_from_earth, :price_per_day, presence: true
+  validates :name, :size, :distance_from_earth, :price_per_day, presence: true
   validates_uniqueness_of :name, on: :create, message: "must be unique"
   validates :size, :distance_from_earth, :price_per_day, on: :create, message: "is not a number", numericality: { only_integer: true }
 end
