970e46aa35a9d8ba3b86a8cf95d144e87038aace
Michael Knott <knottweb@gmail.com>
2020-08-01 12:28:45 +0100

    Changed distance_from_earth to distance_from_sun in the model validations, and the schema/table

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,7 +1,7 @@
 class Planet < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
-  validates :name, :size, :distance_from_earth, :price_per_day, presence: true
+  validates :name, :size, :distance_from_sun, :price_per_day, presence: true
   validates_uniqueness_of :name, on: :create, message: "must be unique"
   validates :size, :distance_from_sun, :price_per_day, numericality: { only_integer: true }
 end
