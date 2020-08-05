26a5e623e2b7c739760c33d50e6acc2acd9a6f2a
Michael Knott <knottweb@gmail.com>
2020-08-05 17:29:10 +0100

    Seed file complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,52 @@
+# User seeds
+users = [
+  renter_user = ['renter@user.com', '123456'],
+  owner_user = ['owner@user.com', '123456']
+]
+
+# 2 user are created with basic '123456' passwords, for the purposes of testing.
+# The new bookings generated in the seed file below, will be assigned to this renter_user, so that the owner_user
+# can see these bookings, and accept them, or the renter_user can view his requested bookings
+
+users.each do |email, password|
+  User.create(email: email, password: password)
+end
+
+
 # Planet seeds
-mercury = Planet.new({name: 'Mercury', size: 2439.7, distance_from_sun: 57.91, price_per_day: 872})
-venus = Planet.new({name: 'Venus', size: 6051.8, distance_from_sun: 108.2, price_per_day: 123})
-mars = Planet.new({name: 'Mars', size: 3389.5, distance_from_sun: 227.9, price_per_day: 222})
-jupiter = Planet.new({name: 'Jupiter', size: 69911, distance_from_sun: 778.5, price_per_day: 947})
-saturn = Planet.new({name: 'Saturn', size: 58232, distance_from_sun: 1434000, price_per_day: 142})
-uranus = Planet.new({name: 'Uranus', size: 25362, distance_from_sun: 2871000, price_per_day: 654})
-neptune = Planet.new({name: 'Neptune', size: 24622, distance_from_sun: 7618000, price_per_day: 787})
-tatooine = Planet.new({name: 'Tatooine', size: 10465, distance_from_sun: 4863000, price_per_day: 15687})
-corusant = Planet.new({name: 'Corusant', size: 12240, distance_from_sun: 73491990, price_per_day: 8746})
-LV = Planet.new({name: 'LV-426', size: 1200, distance_from_sun: 1204004, price_per_day: 69561})
-cybertron = Planet.new({name: 'Cybertron', size: 6300, distance_from_sun: 999999, price_per_day: 654})
-krypton = Planet.new({name: 'Krypton', size: 6370, distance_from_sun: 10000001, price_per_day: 123748})
+
+planets = [
+  ['Mercury', 2440, 58, 872],
+  ['Venus', 6052, 108, 123],
+  ['Mars', 3390, 228, 222],
+  ['Jupiter', 69911, 778.5, 947],
+  ['Saturn', 58232, 1434000, 142],
+  ['Uranus', 25362, 2871000, 654],
+  ['Neptune', 24622, 7618000, 787],
+  ['Tatooine', 10465, 4863000, 15687],
+  ['Corusant', 12240, 73491990, 8746],
+  ['LV-426', 1200, 1204004, 69561],
+  ['Cyberton', 6300, 999999, 654],
+  ['Krypton', 6370, 100001, 123748]  
+]
+
+# The planets will be assigned to the owner_user, based on a clean DB before seeding.
+# This will ensure that booking requests are separate from the owner
+
+planets.each do |name, size, distance_from_sun, price_per_day|
+  Planet.create(name: name, size: size, distance_from_sun: distance_from_sun, price_per_day: price_per_day, user_id: 2)
+end
+
+
+# Booking seeds
+
+planet_list = Planet.all
+
+# 2 bookings is created for every planet that was generated above in the seed file, with the same data.
+# The booking is assigned to the test_user that was created in the seed file above.
+# This is so that the owner is not the renter as well.
+
+planet_list.each do |planet|
+  Booking.create(start_date: Date.parse("Jan 1 2021"), end_date: Date.parse("Jan 2 2021"), planet_id: planet.id, user_id: 1)
+  Booking.create(start_date: Date.parse("Feb 9 2022"), end_date: Date.parse("Feb 10 2022"), planet_id: planet.id, user_id: 1)
+end
