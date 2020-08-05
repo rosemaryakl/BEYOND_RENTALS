6419e5506e7f76265a65fee37e9179d994cfaba1
Michael Knott <knottweb@gmail.com>
2020-08-05 19:18:41 +0100

    Images added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,26 +21,27 @@ end
 
 # Planet seeds
 
+
 planets = [
-  ['Mercury', 2440, 58, 872],
-  ['Venus', 6052, 108, 123],
-  ['Mars', 3390, 228, 222],
-  ['Jupiter', 69911, 778.5, 947],
-  ['Saturn', 58232, 1434000, 142],
-  ['Uranus', 25362, 2871000, 654],
-  ['Neptune', 24622, 7618000, 787],
-  ['Tatooine', 10465, 4863000, 15687],
-  ['Corusant', 12240, 73491990, 8746],
-  ['LV-426', 1200, 1204004, 69561],
-  ['Cyberton', 6300, 999999, 654],
-  ['Krypton', 6370, 100001, 123748]  
+  ['Mercury', 2440, 58, 872, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/mercury_foe8oi.jpg'],
+  ['Venus', 6052, 108, 123, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/venus_ebjfdb.jpg'],
+  ['Mars', 3390, 228, 222, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/mars_zg2snb.jpg'],
+  ['Jupiter', 69911, 778.5, 947, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650598/Planets/jupiter_jig2dr.jpg'],
+  ['Saturn', 58232, 1434000, 142, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/saturn_jhr4ir.jpg'],
+  ['Uranus', 25362, 2871000, 654, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/uranus_b6o9sn.jpg'],
+  ['Neptune', 24622, 7618000, 787, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/neptune_qfdkiu.jpg'],
+  ['Tatooine', 10465, 4863000, 15687, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650597/Planets/tatooine_jehwky.png'],
+  ['Corusant', 12240, 73491990, 8746, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650595/Planets/curosant_lvdfs4.jpg'],
+  ['LV-426', 1200, 1204004, 69561, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/LV426_eb2yza.png'],
+  ['Cyberton', 6300, 999999, 654, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/cybertron_e4bsex.jpg'],
+  ['Krypton', 6370, 100001, 123748, 'https://res.cloudinary.com/dinzvmaaw/image/upload/v1596650596/Planets/krypton_ul1rbx.jpg']  
 ]
 
-# The planets will be assigned to the owner_user, based on a clean DB before seeding.
+# The planets will be assigned to the owner_user.
 # This will ensure that booking requests are separate from the owner
 
-planets.each do |name, size, distance_from_sun, price_per_day|
-  Planet.create(name: name, size: size, distance_from_sun: distance_from_sun, price_per_day: price_per_day, user_id: 2)
+planets.each do |name, size, distance_from_sun, price_per_day, image|
+  Planet.create(name: name, size: size, distance_from_sun: distance_from_sun, price_per_day: price_per_day, image: image, user_id: 2)
 end
 
 
